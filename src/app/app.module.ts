f946c890991024dcb51e068616597c8bf9dca285
Angular CLI <angular-cli@angular.io>
2018-01-29 14:46:57 +0200

    chore: initial commit from @angular/cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { FrontComponent } from './front/front.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { RegisterComponent } from './register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FrontComponent,
+    TopBarComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
